5d1d243d2927c7b065c0ba0d7da51716a54d534b
codev-desenvolvesoftware <codev.desenvolvesoftware@gmail.com>
2025-05-10 14:57:01 -0300

    correção do globals.css no app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 // pages/_app.js
-import '@/styles/globals.css'; // importa o CSS com Tailwind
+import '../styles/globals.css'; // importa o CSS com Tailwind
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
