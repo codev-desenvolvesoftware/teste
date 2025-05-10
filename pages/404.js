abc1a4cf727494cad9fc9490537fb005aae50bfd
codev-desenvolvesoftware <codev.desenvolvesoftware@gmail.com>
2025-05-10 14:51:21 -0300

    add versão node

---
+++ pages/404.js
@@ -0,0 +1,9 @@
+// pages/404.js
+export default function Custom404() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '3rem' }}>
+      <h1>404 - Página não encontrada</h1>
+      <p>Verifique se o endereço está correto.</p>
+    </div>
+  );
+}
---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+// pages/_app.js
+import '@/styles/globals.css'; // importa o CSS com Tailwind
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
